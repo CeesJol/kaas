0693f26429d09316dd25e6c74cbb9b0e0f79f255
Cees <cjol99@icloud.com>
2021-01-15 10:28:08 +0100

    kaas

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+  res.statusCode = 200;
+  res.json({
+    id: "1da80986-1925-58cf-b048-7ccd04a6f6e8",
+    callType: {
+      id: "1da80986-1925-58cf-b048-7ccd04a6f6e8",
+      name: "Storing",
+    },
+    completedDate: "2020-07-14T16:52:00.000+0000",
+    targetDate: "2020-07-14T16:52:00.000+0000",
+    entryType: {
+      name: "Telefonish",
+    },
+  });
+};
