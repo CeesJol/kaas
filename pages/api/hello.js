a36817216608829fdb25aad315b1ddccdccba8fe
Cees <cjol99@icloud.com>
2021-01-15 10:31:46 +0100

    fix

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,16 +2,18 @@
 
 export default (req, res) => {
   res.statusCode = 200;
-  res.json({
-    id: "1da80986-1925-58cf-b048-7ccd04a6f6e8",
-    callType: {
+  res.json([
+    {
       id: "1da80986-1925-58cf-b048-7ccd04a6f6e8",
-      name: "Storing",
+      callType: {
+        id: "1da80986-1925-58cf-b048-7ccd04a6f6e8",
+        name: "Storing",
+      },
+      completedDate: "2020-07-14T16:52:00.000+0000",
+      targetDate: "2020-07-14T16:52:00.000+0000",
+      entryType: {
+        name: "Telefonish",
+      },
     },
-    completedDate: "2020-07-14T16:52:00.000+0000",
-    targetDate: "2020-07-14T16:52:00.000+0000",
-    entryType: {
-      name: "Telefonish",
-    },
-  });
+  ]);
 };
