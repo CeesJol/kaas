36b045bcf446595b3e6d8ac46f0238fcf8a88816
Cees <cjol99@icloud.com>
2021-01-15 10:33:51 +0100

    1000 incidents

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,18 +2,22 @@
 
 export default (req, res) => {
   res.statusCode = 200;
-  res.json([
-    {
+  const NUMBER_OF_INCIDENTS = 1000;
+  const INCIDENT = {
+    id: "1da80986-1925-58cf-b048-7ccd04a6f6e8",
+    callType: {
       id: "1da80986-1925-58cf-b048-7ccd04a6f6e8",
-      callType: {
-        id: "1da80986-1925-58cf-b048-7ccd04a6f6e8",
-        name: "Storing",
-      },
-      completedDate: "2020-07-14T16:52:00.000+0000",
-      targetDate: "2020-07-14T16:52:00.000+0000",
-      entryType: {
-        name: "Telefonish",
-      },
+      name: "Storing",
     },
-  ]);
+    completedDate: "2020-07-14T16:52:00.000+0000",
+    targetDate: "2020-07-14T16:52:00.000+0000",
+    entryType: {
+      name: "Telefonish",
+    },
+  };
+  const result = [];
+  for (var i = 0; i < NUMBER_OF_INCIDENTS; i++) {
+    result.push(INCIDENT);
+  }
+  res.json(result);
 };
